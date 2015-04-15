d83f4c34ce26cefc00eab428c27e3636d39e86ac
Milo de Vries <milo.devries@gimiscale.com>
2015-04-15 03:59:48 +0200

    Working basic setup for lamernews

--- app.tf
+++ app.tf
@@ -4,25 +4,31 @@ resource "digitalocean_droplet" "ln-app" {
   name = "ln-app"
   region = "ams3"
   size = "512mb"
+  private_networking = "true"
   ssh_keys = [ "${digitalocean_ssh_key.deployer.fingerprint}" ]
   connection {
     user = "root"
     key_file = "deploy-key"
   }
+  provisioner "remote-exec" {
+    inline = [
+    /* Create dir */
+    "sudo mkdir -p /opt/ln-docker"
+    ]
+  }
   provisioner "file" {
     source = "app/Dockerfile"
-    destination = "/opt/ln-docker"
+    destination = "/opt/ln-docker/Dockerfile"
   }
   provisioner "remote-exec" {
     inline = [
       /* Install docker */
-      "curl -sSL https://get.docker.com/ubuntu/ | sudo sh",
+      "sudo apt-get update && apt-get -qy install docker.io",
       /* Provide Redis IP to app */
-      "echo ${digitalocean_droplet.ln-redis.ipv4_address} > redis_host",
-      "cat redis_host",
+      "cd /opt/ln-docker && echo ${digitalocean_droplet.ln-redis.ipv4_address_private} > redis_host",
       /* Build container */
-      "cd /opt/ln-docker && sudo docker build -t lame/app .",
-      "sudo docker run --name ln-app -t -i lame/app"
+      "sudo docker build -t lame/app .",
+      "sudo docker run -d -p=80:8080 --name ln-app -t -i lame/app"
     ]
   }
 }
--- redis.tf
+++ redis.tf
@@ -4,6 +4,7 @@ resource "digitalocean_droplet" "ln-redis" {
   name = "ln-redis"
   region = "ams3"
   size = "512mb"
+  private_networking = "true"
   ssh_keys = [ "${digitalocean_ssh_key.deployer.fingerprint}" ]
   connection {
     user = "root"
@@ -12,9 +13,12 @@ resource "digitalocean_droplet" "ln-redis" {
   provisioner "remote-exec" {
     inline = [
       /* Install docker */
-      "curl -sSL https://get.docker.com/ubuntu/ | sudo sh",
+      "sudo apt-get update && apt-get -qy install docker.io",
+      /*"curl -sSL https://get.docker.com/ubuntu/ | sudo sh",*/
       /* Initialize ruby container */
-      "sudo docker run -d --name ln-redis redis"
+      "sudo docker run -d -p=6379:6379 --name ln-redis redis"
+      /* Allow web instance access to Redis */
+      /*"sudo ufw allow proto tcp to ${self.ipv4_address_private} port 6379"*/
     ]
   }
 }
